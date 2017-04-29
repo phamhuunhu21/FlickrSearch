7ce04faeed42aab26d3376a0e785a0ad30204ac3
Kiet Nguyen <funsmurf@Funs-Mac.local>
2017-04-29 21:50:55 +0700

    Done

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Kiet Nguyen on 4/29/17.
+//  Copyright © 2017 Group9_iOSUTE. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+class FlickrPhotoCell : UICollectionViewCell{
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ FlickrSearch/FlickrSearchResults.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrSearchResults.swift
+//  FlickrSearch
+//
+//  Created by Kiet Nguyen on 4/29/17.
+//  Copyright © 2017 Group9_iOSUTE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct FlickrSearchResults {
+    let searchTerm: String
+    let searchResults: [FlickrPhoto]
+}
+
+
